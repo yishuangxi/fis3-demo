11bacce55a8a644cbb40f0ac9d85bd321b67ccdb
yishuangxi <yishuangxi@qiushibaike.com>
2016-05-03 00:22:04 +0800

    fis-conf.js

--- demo-simple/fis-conf.js
+++ demo-simple/fis-conf.js
@@ -1,20 +1,21 @@
-// fis.match('::packager', {
-//   spriter: fis.plugin('csssprites')
-// });
+fis.match('::packager', {
+  spriter: fis.plugin('csssprites')
+});
 
-// fis.match('*', {
-//   useHash: false
-// });
+fis.match('*{js, css, png, jpg, gif}', {
+  useHash: true
+});
 
-// fis.match('*.js', {
-//   optimizer: fis.plugin('uglify-js')
-// });
+fis.match('*.js', {
+  optimizer: fis.plugin('uglify-js'),
+  domain: 'http://www.baidu.com'
+});
 
-// fis.match('*.css', {
-//   useSprite: true,
-//   optimizer: fis.plugin('clean-css')
-// });
+fis.match('*.css', {
+  useSprite: true,
+  optimizer: fis.plugin('clean-css')
+});
 
-// fis.match('*.png', {
-//   optimizer: fis.plugin('png-compressor')
-// });
+fis.match('*.png', {
+  optimizer: fis.plugin('png-compressor')
+});
